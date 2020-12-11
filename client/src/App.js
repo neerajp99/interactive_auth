31fdcb59b5dfbf4df338189e2d68373cae7b7122
Tanuj Sood <tanuj.sood12@gmail.com>
2020-12-11 16:51:03 +0530

    Minor Fix + Bootstrap Test

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import './fonts/DIN Next LT Pro Medium.ttf';
 import './fonts/Lato-Bold.ttf';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Badge } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-    HELLO WORLD
+    HELLO WORLD <Badge variant="secondary"> Wow </Badge>
     </div>
   );
 }
