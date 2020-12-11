14dc268d92bfe47e4a9c3e80616a481097faba56
neerajp99 <neerajp1999@gmail.com>
2020-12-12 03:10:21 +0530

    Modified app

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,41 @@
-import './App.css';
-import './fonts/DIN Next LT Pro Medium.ttf';
-import './fonts/Lato-Bold.ttf';
-import logill from './assets/Login_Illustration.png'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Col, Row, Form, Button } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./Login"
+import SignUp from "./Signup"
+import setAuthToken from "./setAuthToken"
+import jwt_decode from "jwt-decode"
+
+
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  //Decode the tokens ang get user infe ormation
+  const decodedToken = jwt_decode(localStorage.jwtToken);
+  //Check for expired tokens
+  const currentTime = Date.now() / 1000;
+  if (decodedToken.exp < currentTime) {
+    // log out user
+    localStorage.removeItem("jwtToken");
+    // Remove Auth headers from axios request
+    setAuthToken(false);
+    // Set profile to null
+    // store.dispatch(clearProfile());
+    // clear current Profile
+    // TODO:::TODO
+    // Redirect to login
+    window.alert("Session expired, please login again.");
+    window.location.href = "/login";
+  }
+}
+
 
 function App() {
   return (
-    <Row>
-      <Col xs={8} className="image_col">
-        <img className="log_ill" src={logill} alt="login"></img>
-      </Col>
-
-      <Col>
-        <div className="form_box">
-          <h2 className="login-heading">Welcome Back</h2>
-          <p className="login-subheading">To log in please enter your email and password</p>
-          <Form className="new_form">
-            <Form.Group controlId="formBasicEmail">
-            <Form.Control className="form_text" type="email" placeholder="Email" />
-            <Form.Text className="text-muted">
-              <p className="caption">We'll never share your email with anyone else.</p>
-            </Form.Text>
-          </Form.Group>
-
-          <Form.Group controlId="formBasicPassword">
-            <Form.Control className="form_text" type="password" placeholder="Password" />
-          </Form.Group>
-          <Button className="form_button" variant="primary" type="submit">
-            Login
-          </Button>
-        </Form>
-        <p className="check_test">First Time? <a classname="link" href={"https://google.com/"}>Sign Up</a></p>
-        </div>
-      </Col>
-    </Row>
+    <Router>
+          <div className="App">
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          </div>
+    </Router>
   );
 }
 
